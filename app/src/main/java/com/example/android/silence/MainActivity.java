58957365777748936d0edc1082efc7f1e32d9220
Shane Strait <abstractfive05@gmail.com>
2016-12-15 15:46:20 -0600

    Initial commit

---
+++ app/src/main/java/com/example/android/silence/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.android.silence;
+
+import android.location.Location;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+GoogleApiClient.OnConnectionFailedListener, LocationListener{
+
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private SilentLocale launchCode = new SilentLocale(38.651703, -90.2593865);
+    private AudioManager mAudioManager;
+    private Toast toast;
+    private TextView mTxtLatitude;
+    private TextView mTxtLongitude;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        mTxtLatitude = (TextView) findViewById(R.id.latitude);
+        mTxtLongitude = (TextView) findViewById(R.id.longitude);
+        buildGoogleApiClient();
+    }
+
+    @Override
+    protected void onStop() {
+        if(mGoogleApiClient.isConnected()){
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(5000);
+        try{
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }catch(SecurityException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(LOG_TAG, "Connection suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(LOG_TAG, "Connection failed: " + connectionResult.getErrorCode());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mTxtLongitude.setText(Double.toString(location.getLongitude()));
+        mTxtLatitude.setText(Double.toString(location.getLatitude()));
+
+        if(mAudioManager.getRingerMode() != AudioManager.RINGER_MODE_SILENT) {
+            if (launchCode.isSilent(location)) {
+                mAudioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                toast = Toast.makeText(this, "You're silenced!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+        if(mAudioManager.getRingerMode() == AudioManager.RINGER_MODE_SILENT) {
+            if (!launchCode.isSilent(location)) {
+                mAudioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                toast = Toast.makeText(this, "You're free ringing!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
+    public synchronized void buildGoogleApiClient(){
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+}
---
+++ app/src/main/java/com/example/android/silence/SilentLocale.java
@@ -0,0 +1,76 @@
+package com.example.android.silence;
+
+import android.location.Location;
+
+/**
+ * Created by strai on 12/14/2016.
+ */
+
+public class SilentLocale {
+    private double mLat;
+    private double mLon;
+    private double mRadius;
+    private float[] distance = new float[1];
+    private String mName;
+    public static final String SILENCE_TAG = SilentLocale.class.getSimpleName();
+
+    public SilentLocale(double latitude, double longitude){
+        mLat = latitude;
+        mLon = longitude;
+        mRadius = 10.0;
+        mName = "LaunchCode";
+    }
+
+    /**
+     *Returns the latitude of SilentLocale object
+     */
+    public double getSilentLatitude(){
+        return mLat;
+    }
+
+    /**
+     *Returns the longitude of SilentLocale object
+     */
+    public double getSilentLongitude(){
+        return mLon;
+    }
+
+    /**
+     *Returns the radius of SilentLocale object effect
+     */
+    public double getSilentRadius(){
+        return mRadius;
+    }
+
+    /**
+     *Returns the name of the SilentLocale
+     */
+    public String getSilentName(){
+        return mName;
+    }
+
+    /**
+     *Allows the user to set a new value for the radius
+     */
+    public void setSilentRadius(double radius){
+        mRadius = radius;
+    }
+
+    /**
+     *Takes the user's current location and calculates the
+     * distance to the SilentLocale object and returns
+     * true if it is within the radius and false otherwise
+     * @param currentLocation the user's current location
+     */
+    public boolean isSilent(Location currentLocation){
+
+        Location.distanceBetween(currentLocation.getLatitude(), currentLocation.getLongitude(),
+                mLat, mLon, distance);
+
+        if((double)distance[0] < mRadius){
+            return true;
+        }
+
+        return false;
+    }
+}
