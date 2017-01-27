28d24a67296e89ff4e24dbaa7b4b054837325060
Shane Strait <abstractfive05@gmail.com>
2017-01-27 15:52:18 -0600

    Implemented editor save/delete

--- app/src/main/java/com/example/android/silence/EditActivity.java
+++ app/src/main/java/com/example/android/silence/EditActivity.java
@@ -1,20 +1,25 @@
 package com.example.android.silence;
 
 import android.app.LoaderManager;
+import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.silence.data.SilentContract.SilentEntry;
 
@@ -80,14 +85,105 @@ public class EditActivity extends AppCompatActivity implements LoaderManager.Loa
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
             case R.id.action_save:
+                saveLocation();
+                finish();
                 return true;
             case R.id.action_delete_item:
+                showDeleteConfirmationDialog();
                 return true;
-            case R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
+            case android.R.id.home:
+                if(!mLocaleHasChanged){
+                    NavUtils.navigateUpFromSameTask(EditActivity.this);
+                    return true;
+                }
+
+                showUnsavedChangesDialog();
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void saveLocation(){
+        ContentValues values = new ContentValues();
+
+        String name = mEditName.getText().toString().trim();
+        String address = mEditAddress.getText().toString().trim();
+        String radiusText = mEditRadius.getText().toString().trim();
+        int radius = Integer.parseInt(radiusText);
+
+        if(TextUtils.isEmpty(name) && TextUtils.isEmpty(address)){
+            return;
+        }
+
+        values.put(SilentEntry.COLUMN_LOCALE_NAME, name);
+        values.put(SilentEntry.COLUMN_LOCALE_ADDRESS, address);
+        values.put(SilentEntry.COLUMN_LOCALE_RADIUS, radius);
+
+        if(mCurrentLocaleUri == null){
+            getContentResolver().insert(SilentEntry.CONTENT_URI, values);
+        }else{
+            int rowUpdated = getContentResolver().update(mCurrentLocaleUri, values, null, null);
+
+            if(rowUpdated == 0){
+                Toast.makeText(this, "Save failed", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this, "Location Saved", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void deleteLocale(){
+        int rowDeleted = getContentResolver().delete(mCurrentLocaleUri, null, null);
+
+        if(rowDeleted == 0){
+            Toast.makeText(this, "Deletion failed", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Location Deleted", Toast.LENGTH_SHORT).show();
+        }
+
+        finish();
+    }
+
+    public void showDeleteConfirmationDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Delete this Location?");
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                deleteLocale();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    public void showUnsavedChangesDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Discard changes and quit editing?");
+        builder.setPositiveButton("Discard", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                NavUtils.navigateUpFromSameTask(EditActivity.this);
+            }
+        });
+        builder.setNegativeButton("Keep Editing", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(dialog != null){
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 
     @Override
